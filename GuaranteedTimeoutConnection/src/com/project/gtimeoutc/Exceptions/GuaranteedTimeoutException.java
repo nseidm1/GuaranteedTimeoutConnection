f9a5539aee9fb15120a32b48ff01a63d09ead022
nseidm1 <noah@noahseidman.com>
2012-10-02 22:19:15 -0400

    Custom exception.

---
+++ GuaranteedTimeoutConnection/src/com/project/gtimeoutc/Exceptions/GuaranteedTimeoutException.java
@@ -0,0 +1,9 @@
+package com.project.gtimeoutc.Exceptions;
+
+public class GuaranteedTimeoutException extends Exception {
+	private static final long serialVersionUID = 7119314078822372600L;
+
+	public GuaranteedTimeoutException(String exception) {
+		super(exception);
+	}
+}
