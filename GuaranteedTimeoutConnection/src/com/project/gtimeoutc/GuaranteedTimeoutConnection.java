740a62a553a6a41d1938875cb766f383970db99d
nseidm1 <noah@noahseidman.com>
2012-10-02 21:40:22 -0400

    Formatting.

--- GuaranteedTimeoutConnection/src/com/project/gtimeoutc/GuaranteedTimeoutConnection.java
+++ GuaranteedTimeoutConnection/src/com/project/gtimeoutc/GuaranteedTimeoutConnection.java
@@ -1,4 +1,5 @@
 package com.project.gtimeoutc;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,170 +10,156 @@ import com.project.gtimeoutc.callbacks.InputStreamCallback;
 import com.project.gtimeoutc.callbacks.OpenConnectionCallback;
 
 import android.os.Handler;
+
 /**
  * @author Noah Seidman
  */
-public class GuaranteedTimeoutConnection
-{
-    private int mMilliseconds;
-    private Thread mOpenConnectionThread;
-    private boolean mUseSSL;
-    private Handler mHandler = new Handler();
-    private HttpURLConnection mHttpUrlConnection;
-    private HttpsURLConnection mHttpsUrlConnection;
-    private Runnable mTimeoutRunnable = new Runnable()
-    {
-    	@Override
-    	public void run()
-    	{
-    		mHttpUrlConnection.disconnect();
-    		mOpenConnectionThread.interrupt();
-    	}
-    };
-    /**
-     * This library creates an HttpURLConnection with or without SSL; use the
-     * withSSL boolean in the constructor to specify. You can modify the
-     * HttpsURLConnection using the respective get method, clearly if you want
-     * to specify a custom socket factory this is needed. Be cautious that the constructor creates a Handler 
-     * so either call the constructor from the UI thread or use Looper properly.
-     * 
-     * @param milliseconds
-     *            Specify your guaranteed timeout.
-     * @param useSSL
-     *            Specify if SSL will be used.
-     */
-    public GuaranteedTimeoutConnection(int milliseconds, boolean useSSL)
-    {
-	System.setProperty("http.keepAlive", "false");
-	mMilliseconds = milliseconds;
-	mUseSSL = useSSL;
-    }
-    /**
-     * Return the HttpURLConnection
-     * 
-     * @throws WrongModeException
-     *             If you specify true as the useSSL mode, this will throw a
-     *             WrongModeException
-     */
-    public HttpURLConnection getHttpURLConnection() throws WrongModeException
-    {
-	if (mUseSSL)
-	    throw new WrongModeException("You specied useSSL (true) as a param in the constructor");
-	return mHttpUrlConnection;
-    }
-    /**
-     * Return the HttpsURLConnection
-     * 
-     * @throws WrongModeException
-     *             If you specify false as the useSSL mode, this will throw a
-     *             WrongModeException
-     */
-    public HttpURLConnection getHttpsURLConnection() throws WrongModeException
-    {
-	if (mUseSSL)
-	    throw new WrongModeException("You specied useSSL (false) as a param in the constructor");
-	return mHttpsUrlConnection;
-    }
-    /**
-     * This method creates a thread and posts to the supplied callback instead
-     * of returning the inputStream directly from the method; please consider
-     * this during your development.
-     * 
-     * @param inputStreamCallback
-     *            The InputStreamCallback will always be called! It will either
-     *            return the actual inputStream or null and an exception.
-     * @param url
-     *            Supply the url for the connection.
-     */
-    public void getInputStream(final InputStreamCallback inputStreamCallback, final URL url)
-    {
-	mHandler.postDelayed(mTimeoutRunnable, mMilliseconds);
-	mOpenConnectionThread = new Thread()
-	{
-	    @Override
-	    public void run()
-	    {
-		try
-		{
-		    if (mUseSSL)
-		    {
-			mHttpsUrlConnection = (HttpsURLConnection) url.openConnection();
-			mHttpsUrlConnection.connect();
-		    }
-		    else
-		    {
-			mHttpUrlConnection = (HttpURLConnection) url.openConnection();
-			mHttpUrlConnection.connect();
-		    }
-		    mHandler.removeCallbacks(mTimeoutRunnable);
-		    if (mUseSSL)
-		    {
-			inputStreamCallback.getInputStream(mHttpsUrlConnection.getInputStream(), null);
-		    }
-		    else
-		    {
-			inputStreamCallback.getInputStream(mHttpUrlConnection.getInputStream(), null);
-		    }
+public class GuaranteedTimeoutConnection {
+	private int mMilliseconds;
+	private Thread mOpenConnectionThread;
+	private boolean mUseSSL;
+	private Handler mHandler = new Handler();
+	private HttpURLConnection mHttpUrlConnection;
+	private HttpsURLConnection mHttpsUrlConnection;
+	private Runnable mTimeoutRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mHttpUrlConnection.disconnect();
+			mOpenConnectionThread.interrupt();
 		}
-		catch (IOException e)
-		{
-		    inputStreamCallback.getInputStream(null, e);
-		}
-		catch (NullPointerException e)
-		{
-		    inputStreamCallback.getInputStream(null, e);
-		}
-		catch (Exception e)
-		{
-		    inputStreamCallback.getInputStream(null, e);
-		}
-	    }
 	};
-	mOpenConnectionThread.start();
-    }
-    /**
-     * 
-     * This method creates a thread and posts to the supplied callback instead
-     * of returning the inputStream; please consider this during your
-     * development.
-     * 
-     * @param openConnectionCallback
-     *            The OpenConnectionCallback will always be called. It will pass
-     *            true and false if the connection succeeds or fails to open.
-     *            You can subsequently use the get methods to obtain a reference
-     *            to the HttpURLConnection.
-     * @param url
-     *            Supply the url for the connection.
-     */
-    public void openConnection(final OpenConnectionCallback openConnectionCallback, final URL url)
-    {
-	mHandler.postDelayed(mTimeoutRunnable, mMilliseconds);
-	mOpenConnectionThread = new Thread()
-	{
-	    @Override
-	    public void run()
-	    {
-		try
-		{
-		    if (mUseSSL)
-		    {
-			mHttpsUrlConnection = (HttpsURLConnection) url.openConnection();
-			mHttpsUrlConnection.connect();
-		    }
-		    else
-		    {
-			mHttpUrlConnection = (HttpURLConnection) url.openConnection();
-			mHttpUrlConnection.connect();
-		    }
-		    mHandler.removeCallbacks(mTimeoutRunnable);
-		    openConnectionCallback.connectionOpened(true);
-		}
-		catch (Exception e)
-		{
-		    openConnectionCallback.connectionOpened(false);
-		}
-	    }
-	};
-	mOpenConnectionThread.start();
-    }
+
+	/**
+	 * This library creates an HttpURLConnection with or without SSL; use the
+	 * withSSL boolean in the constructor to specify. You can modify the
+	 * HttpsURLConnection using the respective get method, clearly if you want
+	 * to specify a custom socket factory this is needed. Be cautious that the
+	 * constructor creates a Handler so either call the constructor from the UI
+	 * thread or use Looper properly.
+	 * 
+	 * @param milliseconds
+	 *            Specify your guaranteed timeout.
+	 * @param useSSL
+	 *            Specify if SSL will be used.
+	 */
+	public GuaranteedTimeoutConnection(int milliseconds, boolean useSSL) {
+		System.setProperty("http.keepAlive", "false");
+		mMilliseconds = milliseconds;
+		mUseSSL = useSSL;
+	}
+
+	/**
+	 * Return the HttpURLConnection
+	 * 
+	 * @throws WrongModeException
+	 *             If you specify true as the useSSL mode, this will throw a
+	 *             WrongModeException
+	 */
+	public HttpURLConnection getHttpURLConnection() throws WrongModeException {
+		if (mUseSSL)
+			throw new WrongModeException(
+					"You specied useSSL (true) as a param in the constructor");
+		return mHttpUrlConnection;
+	}
+
+	/**
+	 * Return the HttpsURLConnection
+	 * 
+	 * @throws WrongModeException
+	 *             If you specify false as the useSSL mode, this will throw a
+	 *             WrongModeException
+	 */
+	public HttpURLConnection getHttpsURLConnection() throws WrongModeException {
+		if (mUseSSL)
+			throw new WrongModeException(
+					"You specied useSSL (false) as a param in the constructor");
+		return mHttpsUrlConnection;
+	}
+
+	/**
+	 * This method creates a thread and posts to the supplied callback instead
+	 * of returning the inputStream directly from the method; please consider
+	 * this during your development.
+	 * 
+	 * @param inputStreamCallback
+	 *            The InputStreamCallback will always be called! It will either
+	 *            return the actual inputStream or null and an exception.
+	 * @param url
+	 *            Supply the url for the connection.
+	 */
+	public void getInputStream(final InputStreamCallback inputStreamCallback,
+			final URL url) {
+		mHandler.postDelayed(mTimeoutRunnable, mMilliseconds);
+		mOpenConnectionThread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					if (mUseSSL) {
+						mHttpsUrlConnection = (HttpsURLConnection) url
+								.openConnection();
+						mHttpsUrlConnection.connect();
+					} else {
+						mHttpUrlConnection = (HttpURLConnection) url
+								.openConnection();
+						mHttpUrlConnection.connect();
+					}
+					mHandler.removeCallbacks(mTimeoutRunnable);
+					if (mUseSSL) {
+						inputStreamCallback.getInputStream(
+								mHttpsUrlConnection.getInputStream(), null);
+					} else {
+						inputStreamCallback.getInputStream(
+								mHttpUrlConnection.getInputStream(), null);
+					}
+				} catch (IOException e) {
+					inputStreamCallback.getInputStream(null, e);
+				} catch (NullPointerException e) {
+					inputStreamCallback.getInputStream(null, e);
+				} catch (Exception e) {
+					inputStreamCallback.getInputStream(null, e);
+				}
+			}
+		};
+		mOpenConnectionThread.start();
+	}
+
+	/**
+	 * 
+	 * This method creates a thread and posts to the supplied callback instead
+	 * of returning the inputStream; please consider this during your
+	 * development.
+	 * 
+	 * @param openConnectionCallback
+	 *            The OpenConnectionCallback will always be called. It will pass
+	 *            true and false if the connection succeeds or fails to open.
+	 *            You can subsequently use the get methods to obtain a reference
+	 *            to the HttpURLConnection.
+	 * @param url
+	 *            Supply the url for the connection.
+	 */
+	public void openConnection(
+			final OpenConnectionCallback openConnectionCallback, final URL url) {
+		mHandler.postDelayed(mTimeoutRunnable, mMilliseconds);
+		mOpenConnectionThread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					if (mUseSSL) {
+						mHttpsUrlConnection = (HttpsURLConnection) url
+								.openConnection();
+						mHttpsUrlConnection.connect();
+					} else {
+						mHttpUrlConnection = (HttpURLConnection) url
+								.openConnection();
+						mHttpUrlConnection.connect();
+					}
+					mHandler.removeCallbacks(mTimeoutRunnable);
+					openConnectionCallback.connectionOpened(true);
+				} catch (Exception e) {
+					openConnectionCallback.connectionOpened(false);
+				}
+			}
+		};
+		mOpenConnectionThread.start();
+	}
 }
--- GuaranteedTimeoutConnection/src/com/project/gtimeoutc/callbacks/InputStreamCallback.java
+++ GuaranteedTimeoutConnection/src/com/project/gtimeoutc/callbacks/InputStreamCallback.java
@@ -1,6 +1,7 @@
 package com.project.gtimeoutc.callbacks;
+
 import java.io.InputStream;
-public interface InputStreamCallback
-{
-    public void getInputStream(InputStream inputStream, Exception exception);
+
+public interface InputStreamCallback {
+	public void getInputStream(InputStream inputStream, Exception exception);
 }
